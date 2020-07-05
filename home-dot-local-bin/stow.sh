67ff3e2fbca24f056b8097b4ab7c14c7fec2b8a2
Mics <mics@mics.tw>
2020-07-05 19:05:21 +0800

    add my bin files

---
+++ home-dot-local-bin/stow.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+#
+# Easy stow by Mics
+# v0.2
+#
+
+
+# config
+# ============
+
+# where you want to put files
+export PREFIX='.local/bin'
+# put $HOME or /
+export ROOT="$HOME"                
+
+
+# starting
+# ============
+
+export KEY="$1"
+export TARGET=$HOME/$PREFIX
+export SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+exec $SCRIPTPATH/../tools/stow.sh
