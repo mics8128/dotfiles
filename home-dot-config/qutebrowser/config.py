940a0209a7961d268f2fc307002b86c8b580a5b6
Mics <mics@mics.tw>
2020-07-19 18:16:14 +0800

    add qutebrowser config

---
+++ home-dot-config/qutebrowser/config.py
@@ -0,0 +1,17 @@
+# general?
+c.completion.shrink = True
+c.auto_save.session = True
+c.content.notifications = False
+c.content.pdfjs = True
+
+# tabs
+c.tabs.position = "left"
+c.tabs.width = 200
+c.tabs.show = "multiple"
+
+# dark mode
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.policy.images = "smart"
+c.colors.webpage.darkmode.threshold.text = 150
+c.colors.webpage.darkmode.threshold.background = 205
+c.colors.webpage.prefers_color_scheme_dark = True
